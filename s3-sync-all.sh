c44a249239f69f4ee8e4f366844f0486e58ba46d
Danny Apostoli <dapostoli@sellars.com>
2023-04-04 10:37:40 -0500

    updated folder structure

--- s3-sync-all.sh
+++ s3-sync-all.sh
@@ -1,3 +1,3 @@
-aws s3 sync ./customers/ s3://dapostoli-stedi-lakehouse/customer/landing
+aws s3 sync ./customers/ s3://dapostoli-stedi-lakehouse/customers/landing
 aws s3 sync ./accelerometer/ s3://dapostoli-stedi-lakehouse/accelerometer/landing
 aws s3 sync ./step_trainer/ s3://dapostoli-stedi-lakehouse/step_trainer/landing
